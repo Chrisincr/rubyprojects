6ed1e2658fd72cccf25c1868b9401fc1a3342ea7
Your Name <you@example.com>
2019-03-18 15:57:11 -0500

    student dashboard and .gitignore for .vagrant

---
+++ vagrant_box/student_dashboard/config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  resources :students
+  resources :dojos
+
+  get "dojos/:id/students/new", to: 'students#new',as: 'new_dojo_student'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
---
+++ vagrant_box/student_dashboard/test/system/dojos_test.rb
@@ -0,0 +1,49 @@
+require "application_system_test_case"
+
+class DojosTest < ApplicationSystemTestCase
+  setup do
+    @dojo = dojos(:one)
+  end
+
+  test "visiting the index" do
+    visit dojos_url
+    assert_selector "h1", text: "Dojos"
+  end
+
+  test "creating a Dojo" do
+    visit dojos_url
+    click_on "New Dojo"
+
+    fill_in "Branch", with: @dojo.branch
+    fill_in "City", with: @dojo.city
+    fill_in "State", with: @dojo.state
+    fill_in "Street", with: @dojo.street
+    click_on "Create Dojo"
+
+    assert_text "Dojo was successfully created"
+    click_on "Back"
+  end
+
+  test "updating a Dojo" do
+    visit dojos_url
+    click_on "Edit", match: :first
+
+    fill_in "Branch", with: @dojo.branch
+    fill_in "City", with: @dojo.city
+    fill_in "State", with: @dojo.state
+    fill_in "Street", with: @dojo.street
+    click_on "Update Dojo"
+
+    assert_text "Dojo was successfully updated"
+    click_on "Back"
+  end
+
+  test "destroying a Dojo" do
+    visit dojos_url
+    page.accept_confirm do
+      click_on "Destroy", match: :first
+    end
+
+    assert_text "Dojo was successfully destroyed"
+  end
+end
